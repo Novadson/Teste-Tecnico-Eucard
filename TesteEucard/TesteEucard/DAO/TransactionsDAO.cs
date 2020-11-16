1a40fe33d863cc6f87826be3a743719cee6852fb
Novadson <bloodson1989@gmail.com>
2020-11-16 20:05:55 -0300

    Último commit

--- TesteEucard/TesteEucard/DAO/TransactionsDAO.cs
+++ TesteEucard/TesteEucard/DAO/TransactionsDAO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using TesteEucard.Entitades;
 
 namespace TesteEucard.DAO
@@ -26,10 +27,11 @@ namespace TesteEucard.DAO
 
             Console.Write($"\nInforme o identificador único:");
             _transactions.Identificador = Convert.ToInt32(Console.ReadLine());
-
             _transactions.DataDeCriação = DateTime.Now;
-
             _transactions.ValorParcela = _transactions.ValorTotal.HasValue ? _transactions.ValorTotal / 2 : null;
+
+            if (_ListTransactions.Count > 0 & _ListTransactions.GroupBy(x => x.Identificador).Any(g => g.Count() > 1))
+                Console.WriteLine("O identificador único informado já existe");
             _ListTransactions.Add(_transactions);
             SaveTransactions();
         }
@@ -39,8 +41,18 @@ namespace TesteEucard.DAO
         #region SAVETRANSACTIONS
         public static bool SaveTransactions()
         {
+            ValidarDuplicidade();
             return _ListTransactions.Count > 0 ? true : false;
         }
+
+        public static void ValidarDuplicidade()
+        {
+            Console.WriteLine("Deseja fazer uma nova transação? Digite S para SIM/N para NÃO");
+            string resposta = Console.ReadLine();
+            if (resposta.Equals("S"))
+                SetTransactions();
+        }
+
         #endregion SAVETRANSACTIONS
 
     }
