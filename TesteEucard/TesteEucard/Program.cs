05c408656846be929e34b8bd85ad60587759421c
Novadson <bloodson1989@gmail.com>
2020-11-16 13:37:56 -0300

    Teste Tecnico Eucard

---
+++ TesteEucard/TesteEucard/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using TesteEucard.Entitades;
+
+namespace TesteEucard
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Eucard eucard = new Eucard();
+
+            for (int i = 0; i < eucard.numberLenght; i++)
+            {
+                Console.WriteLine("Informe um número:");
+                eucard.media.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            eucard.result = eucard.media.Average();
+            Console.WriteLine($"A media dos valores é:" + eucard.result);
+
+        }
+    }
+}
